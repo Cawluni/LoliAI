6e0881277137d67ac4372eb0d99cb8353fbc5d09
Naru <35998552+Cawluni@users.noreply.github.com>
2018-08-15 20:51:30 -0400

    Update LoliAI.py

--- LoliAI.py
+++ LoliAI.py
@@ -1,54 +1,177 @@
-#!/usr/bin/env python
+ # version 0.1.6
+import loli, discord, time, random # Time is just used for a timeout.
+from discord.ext import commands
 
-# Main script for LoliAI #
-# When wanting to run LoliAI, run this file #
-# To do =>
+bot_key = loli.bkey
+bot = commands.Bot(command_prefix='l!',description='',pm_help=True)
+bot.remove_command('help') #Redefine this.
+client = discord.Client()
+help = loli.help
+players = {}
+queue = {}
+def check_queue(id):
+    if queue[id] != []:
+        players = queue[id].pop(0)
+        players[id] = players
+        players.start()
+
+@bot.listen()
+async def on_message(message):
+    print(message.author, ":", message.content, "| said in:", message.channel)
+
+@bot.command()
+async def cmds():
+    coms = loli.commands
+    await bot.say(coms)
+
+@bot.command(pass_context=True)
+async def play(ctx, *, url):
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+    players[server.id] = player
+    player.start()
+
+@bot.command(pass_context=True)
+async def play(ctx, *, url):
+    m = ctx.message
+    if 'https://youtube.com/' in m:
+        msg = ctx.message
+        channel = msg.author.voice.voice_channel
+        await bot.join_voice_channel(channel)
+        server = msg.server
+        voice_client = bot.voice_client_in(server)
+        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+        players[server.id] = player
+        player.start()
+    else:
+        msg = ctx.message
+        
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = bot.voice_client_in(server)
+    await voice_client.disconnect()
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    id = ctx.message.server.id
+    players[id].resume()
+
+@bot.command(pass_context=True)
+async def queue(ctx, url):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+    if server.id in queue:
+        queue[server.id].append(player)
+    else:
+        queue[server.id] = [player]
+    await bot.say("Video was added to queue")
+
+@bot.command(pass_context=True)
+async def gay(ctx):
+    url = "https://www.youtube.com/watch?v=hGA-Qid-ChI"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+
+@bot.command(pass_context=True)
+async def communisumlove(ctx):
+    url = "https://www.youtube.com/watch?v=U06jlgpMtQs"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
 
 
-import time,os,platform
-loliMEM_color_settings = False # False means that loli mem cant use color for debug use
-gloli_Color_settings = False # General Color ability, if true color portion will be activated. if not it wont be.
-clear = lambda: os.system('cls') # Clears Terminal Screen
-opsys = platform.system()
-def init():
-    Frames = ['|','/','-','\\'] # Frams for animation
-    msg = "Now loading..."
-    loading = True
-    current = 1 # The current frame Frames[current]
-    mods = ['colors','platform','memory']
-    n = 1
-    # The actual animation/loading #
-    while loading == True:
-        print(msg+"".join(Frames[current])) # Should print out Msg +Current frame. Visually looks like: LoliAI is now loading.../
-        time.sleep(0.1) # 0.1 allows for a super smooth animation!
-        current += 1
-        clear()
-        if n == 1:
-            if (opsys == 'Windows'):
-                from termcolor import colored, cprint
-                from colorama import init
-                init(autoreset=True)
-                print(('Colors module') , ("["),((cprint("Loaded","green")),("]")))
-                a = cprint("LOADED","green") # Format
-                n = 2
-                loliMEM_color_settings = True # Now colors for Debug
-                gloli_color_settings = True # Colors Enabled for General use
-            else:
-                print("Cannot use colors")
-                n = 2
-        if n == 2:
-            from loliAI import tempMEM
-            loli
-        if current >= len(Frames):
-            current = 1
+@bot.command(pass_context=True)
+async def youtube(ctx):
+    url = "https://www.youtube.com/watch?v=brNGDA5R048"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
 
-        # I need to add the fact that it needs to go through eaah thing
+@bot.command(pass_context=True)
+async def loliporn(ctx):
+    url = "https://www.youtube.com/watch?v=LO1Z8HOGeWo"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+
+@bot.command(pass_context=True)
+async def brotherscock(ctx):
+    url = "https://www.youtube.com/watch?v=TBA7Hjch4tY"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+
+@bot.command(pass_context=True)
+async def ilikemen(ctx):
+    url = "https://www.youtube.com/watch?v=NYg96m-7rPA"
+    msg = ctx.message
+    channel = msg.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    server = msg.server
+    voice_client = bot.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
 
 
+@bot.command(pass_context=True)
+async def stop(ctx):
+    id = ctx.message.server.id
+    players[id].stop()
 
+@bot.command()
+async def invite():
+    await bot.say(
+        "Y-You are inviting me!?!?!! E-Eh-EHHH!?!.....W-Well if you insist...: https://discordapp.com/api/oauth2/authorize?client_id=408817025876623361&permissions=8&scope=bot")
 
-def main():
-    init()
+@bot.event
+async def on_ready():
+    print('[INFO]]Loli AI is now running')
+    print('[INFO] Version',loli.version)
+    await bot.change_presence(game=discord.Game(name='Version: '+loli.version))
 
-if __name__ == '__main__':
-    main()
+@bot.command(pass_context=True)
+async def volume(ctx, volume):
+    await bot.say('You just got pranked! I cant do this yet :D')
+
+## REDO EVERYHTHING MUSIC BASED IN ONE AREA/SECTION OR A SEPERATE SCRIPT!
+# Its to cluttered ;-;
+bot.run(bot_key)
